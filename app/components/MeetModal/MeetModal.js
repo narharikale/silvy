3350857caa02ff25331537b00aaf5f9b5ace31ba
narharikale <n7kale@gmail.com>
2025-03-30 19:24:26 +0530

    refactor: standardize code formatting in MeetModal component and its styles

--- app/components/MeetModal/MeetModal.js
+++ app/components/MeetModal/MeetModal.js
@@ -1,17 +1,17 @@
-'use client'
+"use client";
 
-import { Meet } from '../Meet/Meet';
-import styles from './MeetModal.module.css'
+import { Meet } from "../Meet/Meet";
+import styles from "./MeetModal.module.css";
 
 export default function MeetModal({ isOpen, onClose }) {
-    if (!isOpen) return null;
+  if (!isOpen) return null;
 
-    return (
-        <>
-            <div className={styles.overlay} onClick={onClose}></div>
-            <div className={styles.modal}>
-                <Meet onClose={onClose}/>
-            </div>
-        </>
-    )
-}   
+  return (
+    <>
+      <div className={styles.overlay} onClick={onClose}></div>
+      <div className={styles.modal}>
+        <Meet onClose={onClose} />
+      </div>
+    </>
+  );
+}
